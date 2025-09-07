c3773ae5f28c38a2c79ff4965530a18716e18b5f
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-09-07 20:01:49 +0530

    update image export

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,15 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-    ],
-  },
+ images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+    },
+  ],
+  unoptimized: true,   // ⬅️ add this line
+},
   output: 'export',
 };
 
